6842cff35a3a42f082c468de1a41d8bd81ec7c51
Abhinav <Abhinav@Abhinav-PC>
2022-02-22 19:30:42 +0530

    Changes are to Home Page

---
+++ src/test/java/TestCases/HomePage.java
@@ -0,0 +1,17 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+import PageObject.PageObjectInterface;
+
+public class HomePage extends BaseClass implements PageObjectInterface{
+	
+	
+	@Test
+	
+	public void HomeTestcase() {
+		
+		System.out.println("Branching succesfull");
+		
+	}
+	}
